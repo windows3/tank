951bf542c1a970c4a7575f40120a7c688c27b09f
windows3 <416400304@qq.com>
2020-05-07 02:02:46 +0800

    子弹换成图片

--- src/com/zsn/tank/Bullet.java
+++ src/com/zsn/tank/Bullet.java
@@ -28,10 +28,21 @@ public class Bullet {
         if (!live) {
             tf.bullets.remove(this);
         }
-        Color c = g.getColor();
-        g.setColor(Color.RED);
-        g.fillRect(x, y, WIDTH, HEIGHT);
-        g.setColor(c);
+        switch (dir){
+            case LEFT:
+                g.drawImage(ResourceMgr.bulletL,x,y,null);
+                break;
+            case RIGHT:
+                g.drawImage(ResourceMgr.bulletR,x,y,null);
+                break;
+            case UP:
+                g.drawImage(ResourceMgr.bulletU,x,y,null);
+                break;
+            case DOWN:
+                g.drawImage(ResourceMgr.bulletD,x,y,null);
+                break;
+        }
+
         move();
 
     }
